2524c550615e3eee41a6c440e2b8a13f04fae038
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-11-02 00:56:06 -0300

    Add basic functionality to the button

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 'use strict'
 
-let myLibrary = []
+let myLibrary = [
+  {
+    title: 'The Witcher',
+    author: 'Sapkowski',
+    pages: 743,
+    read: false
+  },
+  {
+    title: 'El nombre de la rosa',
+    author: 'Umberto Eco',
+    pages: 861,
+    read: true
+  }
+];
 
 const grid = document.getElementById('library-grid');
 
@@ -24,36 +37,55 @@ Book.prototype.toggleRead = function () {
   this.read = !(this.read);
 }
 
+function createBookCard(book) {
+  const newBook = document.createElement('article');
+  newBook.classList.add('book-card');
+
+  const newBookTitle = document.createElement('h2');
+  newBookTitle.classList.add('book-title');
+  newBookTitle.textContent = book.title;
+
+  const newBookAuthor = document.createElement('p');
+  newBookAuthor.classList.add('book-author');
+  newBookAuthor.textContent = book.author;
+
+  const newBookPages = document.createElement('p');
+  newBookPages.classList.add('book-pages');
+  newBookPages.textContent = `${book.pages} pages`;
+
+  const newBookStatus = document.createElement('p');
+  newBookStatus.classList.add('book-status');
+  newBookStatus.textContent = (book.read) ? "Read" : "Not read yet";
+
+  newBook.appendChild(newBookTitle);
+  newBook.appendChild(newBookAuthor);
+  newBook.appendChild(newBookPages);
+  newBook.appendChild(newBookStatus);
+
+  grid.appendChild(newBook);
+}
+
 function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  createBookCard(newBook);
 }
 
 function displayBooks() {
   for (const book of myLibrary) {
-    const newBook = document.createElement('article');
-    newBook.classList.add('book-card');
-
-    const newBookTitle = document.createElement('h2');
-    newBookTitle.classList.add('book-title');
-    newBookTitle.textContent = book.title;
-
-    const newBookAuthor = document.createElement('p');
-    newBookAuthor.classList.add('book-author');
-    newBookAuthor.textContent = book.author;
-
-    const newBookPages = document.createElement('p');
-    newBookPages.classList.add('book-pages');
-    newBookPages.textContent = `${book.pages} pages`;
-
-    const newBookStatus = document.createElement('p');
-    newBookStatus.classList.add('book-status');
-    newBookStatus.textContent = (book.read) ? "Read" : "Not read yet";
-
-    newBook.appendChild(newBookTitle);
-    newBook.appendChild(newBookAuthor);
-    newBook.appendChild(newBookPages);
-    newBook.appendChild(newBookStatus);
-
-    grid.appendChild(newBook);
+    createBookCard(book);
   }
 }
+
+const addBookButton = document.getElementById('add-book');
+addBookButton.addEventListener('click', () => {
+    const title = prompt('Title?');
+    const author = prompt('Author?');
+    const pages = prompt('Pages?');
+    const read = (prompt('Did you read it?') === 'true') ? true : false;
+
+    addBookToLibrary(title, author, pages, read);
+  }
+);
+
+displayBooks();
