4de39983660284e32d4ad1ea2338c872d61a5096
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-11-08 01:11:15 -0300

    Add functionality to add and remove books

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 'use strict'
 
-let myLibrary = [
-  {
-    title: 'The Witcher',
-    author: 'Sapkowski',
-    pages: 743,
-    read: false
-  },
-  {
-    title: 'El nombre de la rosa',
-    author: 'Umberto Eco',
-    pages: 861,
-    read: true
-  }
-];
+let myLibrary = [];
 
 const grid = document.getElementById('library-grid');
 
@@ -25,12 +12,9 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.info = function () {
-  if (this.read) {
-    return `${this.title} by ${this.author}, ${this.pages} pages, read`;
-  }
-  else {
-    return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
-  }
+  const bookStatus = (this.read) ? 'read' : 'not read yet';
+  
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${bookStatus}`;
 }
 
 Book.prototype.toggleRead = function () {
@@ -38,29 +22,61 @@ Book.prototype.toggleRead = function () {
 }
 
 function createBookCard(book) {
+  if (book === null) return;
+
+  const newBookIndex = myLibrary.indexOf(book);
+
   const newBook = document.createElement('article');
   newBook.classList.add('book-card');
+  newBook.setAttribute('data-index', newBookIndex);
 
   const newBookTitle = document.createElement('h2');
   newBookTitle.classList.add('book-title');
   newBookTitle.textContent = book.title;
+  newBookTitle.setAttribute('data-index', newBookIndex);
 
   const newBookAuthor = document.createElement('p');
   newBookAuthor.classList.add('book-author');
   newBookAuthor.textContent = book.author;
+  newBookAuthor.setAttribute('data-index', newBookIndex);
 
   const newBookPages = document.createElement('p');
   newBookPages.classList.add('book-pages');
   newBookPages.textContent = `${book.pages} pages`;
+  newBookPages.setAttribute('data-index', newBookIndex);
 
   const newBookStatus = document.createElement('p');
   newBookStatus.classList.add('book-status');
   newBookStatus.textContent = (book.read) ? "Read" : "Not read yet";
+  newBookStatus.setAttribute('data-index', newBookIndex);
+
+  const toggleReadButton = document.createElement('button');
+  toggleReadButton.textContent = (book.read) ? "Mark as unread" : "Mark as read";
+  toggleReadButton.classList.add('toggle-status-btn');
+  toggleReadButton.setAttribute('data-index', newBookIndex);
+
+  const deleteBookButton = document.createElement('button');
+  deleteBookButton.textContent = 'Delete book from library';
+  deleteBookButton.classList.add('delete-book-btn');
+  deleteBookButton.setAttribute('data-index', newBookIndex);
 
   newBook.appendChild(newBookTitle);
   newBook.appendChild(newBookAuthor);
   newBook.appendChild(newBookPages);
   newBook.appendChild(newBookStatus);
+  newBook.appendChild(toggleReadButton);
+  newBook.appendChild(deleteBookButton);
+
+  toggleReadButton.addEventListener('click', () => {
+    book.read = !book.read;
+    toggleReadButton.textContent = (book.read) ? "Mark as unread" : "Mark as read";
+    newBookStatus.textContent = (book.read) ? "Read" : "Not read yet";
+  });
+
+  deleteBookButton.addEventListener('click', () => {
+    newBook.remove();
+    myLibrary.splice(newBookIndex, 1, null);
+  });
 
   grid.appendChild(newBook);
 }
@@ -78,14 +94,23 @@ function displayBooks() {
 }
 
 const addBookButton = document.getElementById('add-book');
-addBookButton.addEventListener('click', () => {
-    const title = prompt('Title?');
-    const author = prompt('Author?');
-    const pages = prompt('Pages?');
-    const read = (prompt('Did you read it?') === 'true') ? true : false;
+const formContainer= document.getElementById('form-container');
+const form = document.getElementById('add-book-form');
+const formButton = document.getElementById('form-button');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+const isReadInput = document.getElementById('read');
+const isUnreadInput = document.getElementById('unread');
 
-    addBookToLibrary(title, author, pages, read);
-  }
-);
+addBookButton.addEventListener('click', () => formContainer.classList.toggle('hidden'));
+
+formButton.addEventListener('click', e => {
+  e.preventDefault();
+  if (!titleInput.value || !authorInput.value || !pagesInput.value ||
+      (!isReadInput.checked && !isUnreadInput.checked)) return;
+  addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, isReadInput.checked);
+  form.reset();
+});
 
 displayBooks();
